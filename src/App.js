76b68eb78d85518ed8b389957782f30bf7ddf1b1
AbbasSaad27 <xenozb@gmail.com>
2021-06-26 13:01:21 +0600

    added sign in and sign up page. Made form component and styled it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const shopContent = ({match}) => {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route exact path="/" component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
         <Route path="/shop/:shopName" component={shopContent}/>
+        <Route path="/signin" component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
