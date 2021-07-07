1e76262071c8f939f2a01b8b73323718a044ad7d
AbbasSaad27 <xenozb@gmail.com>
2021-07-07 13:23:20 +0600

    added redux and implement userReducer and rootReducer and store :D

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
@@ -18,5 +19,11 @@ const Header = ({currentUser}) => {
         </div>
     );
 }
- 
-export default Header;
+
+const mapStateToProps = (state) => {
+    return {
+        currentUser: state.user.currentUser
+    }
+}
+
+export default connect(mapStateToProps)(Header);
