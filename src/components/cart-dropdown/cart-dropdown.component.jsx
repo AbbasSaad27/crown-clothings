bf32ffa94a0c4245fccc606c9f450ab3a76739cf
AbbasSaad27 <xenozb@gmail.com>
2021-07-09 12:12:19 +0600

    added cartIcon and cartDropdown and also added toggle functionality. Added cartReducer :D

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import CustomBtn from '../custom-btn/custom-btn.component';
+import './cart-dropdown.styles.scss';
+
+
+
+const CartDropDown = () => {
+    return (
+        <div className="cart-dropdown">
+            <div className="cart-items"></div>
+            <CustomBtn>GO TO CHECKOUT</CustomBtn>
+        </div>
+    );
+}
+
+export default CartDropDown;
